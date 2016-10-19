7810d9eda94836873c9adcc34763de98dcb08fc9
Xavier Boubert <xavier@boubert.fr>
2016-10-19 11:50:54 +0200

    fix(ractive gulpfile): add relative path to $gulp.src()

--- features/ractive/ractive-gulpfile.js
+++ features/ractive/ractive-gulpfile.js
@@ -4,11 +4,11 @@ module.exports = function($gulp) {
   $gulp.task('ractive', function(done) {
 
     $gulp.src([
-      'node_modules/ractive/ractive.js',
-      'node_modules/ractive/ractive.js.map',
-      'node_modules/ractive/ractive.min.js',
-      'node_modules/ractive/ractive.min.js.map',
-      'node_modules/ractive-require/dist/*'
+      './node_modules/ractive/ractive.js',
+      './node_modules/ractive/ractive.js.map',
+      './node_modules/ractive/ractive.min.js',
+      './node_modules/ractive/ractive.min.js.map',
+      './node_modules/ractive-require/dist/*'
     ])
       .pipe($gulp.dist('vendor'))
       .on('end', done);
